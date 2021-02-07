ad10bcd18758cbd2d56b4323cbc97178677a9a04
Bartlomiej Fatyga <batek254@gmail.com>
2021-02-07 21:36:37 +0100

    adding new directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Science Project based data from Drivendata "Pump it Up" Challenge',
+    author='Bartłomiej Fatyga',
+    license='',
+)

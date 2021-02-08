21946591e206c3bdb7809ee2ad2e207f686c8b21
Bartlomiej Fatyga <batek254@gmail.com>
2021-02-08 19:00:02 +0100

    adding documentation and initial notebook

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -2,9 +2,16 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 
-def choose_columns(data, columns:list):
-    '''This function just choose columns from data set
-    '''
+def choose_columns(data, columns:list):    
+    '''This function just choose columns from dataset
+
+    Args:
+        data (pd.DataFrame): dataset
+        columns (list): columns to choose from data
+
+    Returns:
+        data_processed (pd.DataFrame): data frame with choosen columns
+    '''    
     data_processed = data[columns]
 
     return data_processed
@@ -12,7 +19,16 @@ def choose_columns(data, columns:list):
 def treat_nan(data, columns:list, change_from:list, change_to:list):
     '''Function for selected columns using pd.DataFrame.replace change values (from) 
     to the new one (to)
-    '''
+
+    Args:
+        data (pd.DataFrame): dataset
+        columns (list): columns in which values will be replaced
+        change_from (list): from which values replace
+        change_to (list): to which values replace
+
+    Returns:
+        pd.DataFrame: dataset
+    '''    
     for c, f, t in zip(columns, change_from, change_to):
         data[c].replace(f, t)
     
